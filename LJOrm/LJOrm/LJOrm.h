05e7491d746974cfadb5ab40fb1955b3d3702baa
Lucky <lucky.liu@runwise.cn>
2017-11-29 13:36:00 +0800

    init

---
+++ LJOrm/LJOrm/LJOrm.h
@@ -0,0 +1,13 @@
+//
+//  LJOrm.h
+//  LJOrm
+//
+//  Created by 刘瑾 on 2017/11/29.
+//  Copyright © 2017年 刘瑾. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJOrm : NSObject
+
+@end
---
+++ LJOrm/LJOrm/LJOrm.m
@@ -0,0 +1,13 @@
+//
+//  LJOrm.m
+//  LJOrm
+//
+//  Created by 刘瑾 on 2017/11/29.
+//  Copyright © 2017年 刘瑾. All rights reserved.
+//
+
+#import "LJOrm.h"
+
+@implementation LJOrm
+
+@end
